fb951019d58ebc420a8759a7a6bba17a75eaf557
zackia <Zainzakia99@gmail.com>
2024-02-13 09:16:14 +0700

    styling login

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 
 const HomeScreen = () => {
     return(
-        <View>
-            <Text>Selamat Datang di Permainan</Text>
+        <View style={styles.container}>
+            <Text style={styles.welcomeText}>Selamat Datang di Permainan</Text>
         </View>
     );
 };
@@ -16,9 +14,16 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#e0aed0',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  welcomeText: {
+    color: '#756ab6',
+    fontSize: 35,
+    fontWeight: 'bold',
+    alignItems: 'center',
   },
 });
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,38 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, TouchableOpacity, View, Button, TextInput } from 'react-native';
 import React, {useState} from 'react';
 import axios from 'axios';
+//import PopUpSignup from '../components/PopUpSignup';
 
 const LoginScreen = ({navigation}) => {
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+
+    const [status, setStatus] = useState(500);
+    const [message, setMessage] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
 
     const handleLogin = async () => {
         try{
-            const respons = await axios.post('Login BE', {username, password});
+            const respons = await axios.post('http://localhost:5000/api/login', {username, password});
+            console.log(respons);
+            setStatus(respons.status);
+            setMessage(respons.data.message);
+
+            if (respons.status === 404 || respons.status === 401) {
+                setModalVisible(true)
+                console.log('Username atau Password tidak valid');
+            }
+
             const token = respons.data.token;
-            navigation.navigate('Home');
+            setToken(token);
+            console.log('Token:', token);
+            navigation.navigate('Home', { token, username });
         } catch (error) {
             console.error('Login Gagal', error);
         }
     };
 
+    const handleCloseModal = () => {
+        setModalVisible(false);
+      };
+      
     return(
-        <View>
+        <View style={styles.container}>
+            <Text style={styles.hiText}>Alo!!! {'\n'}</Text>
             <TextInput
+                style={styles.input}
                 placeholder='Masukkan Username'
                 value={username}
                 onChangeText={setUserName}
             />
             <TextInput
+                style={styles.input}
                 placeholder='Masukkan Password'
                 value={password}
                 onChangeText={setPassword}
+                secureTextEntry
             />
-            <Button title='Login' onPress={handleLogin}/>
-            <Text> Belum memiliki akun?</Text>
+            <TouchableOpacity style={styles.loginButton}  onPress={handleLogin}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.signButton} onPress={() => navigation.navigate('Signup')}>
+                <Text style={styles.buttonText}>{'\n'}Belum memiliki akun?</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -40,10 +67,47 @@ const LoginScreen = ({navigation}) => {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      backgroundColor: '#e0aed0',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    signButton: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    input: {
+      height: 40,
+      width: '100%',
+      borderColor: '#756ab6',
+      borderWidth: 1,
+      marginBottom: 20,
+      paddingLeft: 10,
+    },
+    loginButton: {
+      backgroundColor: '#ffe5e5',
+      padding: 10,
+      borderRadius: 5,
+      width: '100%',
+      alignItems: 'center'
+    },
+    loginText: {
+        color: '#756ab6',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        position: 'relative'
+      },
+    buttonText: {
+      color: '#ffe5e5',
+      alignItems: 'center',
+    },
+    hiText: {
+        color: '#756ab6',
+        fontSize: 35,
+        fontWeight: 'bold',
+        alignItems: 'center',
+      },
+  });
+  

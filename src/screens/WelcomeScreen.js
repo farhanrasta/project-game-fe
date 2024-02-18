a9a0247dee8f3bf8a1a7b5a8c6aac06d3c4ffac2
auliakharis <56850529+auliakharis@users.noreply.github.com>
2024-02-18 07:57:33 +0700

    tambah logo logout

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -3,12 +3,22 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from 'react-na
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
 import gb2 from '../assets/gb2.png'
+import PopUpLogout from '../components/PopUpLogout';
+import settingImage from '../assets/logout.png';
+
+
 
 const ProfileScreen = ({ route }) => {
   const { username, token } = route.params;
   const [greeting, setGreeting] = useState('');
   const navigation = useNavigation(); // Get navigation object
   const [name, setName] = useState('');
+  const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+
+  useEffect(() => {
+    navigation.setParams({ toggleModal: () => setLogoutModalVisible(true) });
+}, false);
+
 
   useEffect(() => {
     const fetchGreeting = async () => {
@@ -34,6 +44,33 @@ const ProfileScreen = ({ route }) => {
   
   };
 
+  const handleLogout = async () => {
+    // Perform logout operation here
+    // For example: navigation.navigate('Login');
+    try {
+        const response = await axios.delete(
+            `https://joey-pet-minnow.ngrok-free.app/api/logout/${username}`, // Sesuaikan dengan URL endpoint logout di backend Anda
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        // Clear local storage, reset states, navigate to login, etc.
+        if (response.status === 200) {
+            navigation.navigate('Login');
+            console.log('Logout Success');
+        } else {
+            console.log('Logout Failed');
+        }
+    } catch (error) {
+        console.error('Logout failed:', error);
+        // Handle logout failure, display error message, etc.
+    }
+    setLogoutModalVisible(false); // Close the logout modal
+};
+
   return (
     <View style={styles.container}>
         <Image source={gb2} style={styles.image} />
@@ -42,7 +79,20 @@ const ProfileScreen = ({ route }) => {
       <TouchableOpacity style={styles.button} onPress={handleStartGame}>
         <Text style={styles.buttonText}>Let's Start</Text>
       </TouchableOpacity>
-    </View>
+               <View style={styles.settingContainer}>
+                   <TouchableOpacity
+                       style={styles.settingButton}
+                       onPress={() => setLogoutModalVisible(true)}
+                   >
+                       <Image source={settingImage} style={styles.logo} />
+                   </TouchableOpacity>
+                   </View>
+                   <PopUpLogout 
+                    visible={logoutModalVisible}
+                    onClose={() => setLogoutModalVisible(false)}
+                    handleLogout={handleLogout}
+                    />
+                   </View>
   );
 };
 
@@ -67,6 +117,19 @@ const styles = StyleSheet.create({
       fontSize: 16,
       fontWeight: 'bold',
     },
+    settingContainer: {
+      position: 'absolute',
+      flexDirection: 'row',
+      top: 20,
+      right: 20,
+  }, 
+  logo: {
+    width: 30,
+    height: 30,
+    resizeMode: 'contain',
+    justifyContent: 'flex-end' ,
+    marginLeft: 5,
+},
   });
   
   export default ProfileScreen;
